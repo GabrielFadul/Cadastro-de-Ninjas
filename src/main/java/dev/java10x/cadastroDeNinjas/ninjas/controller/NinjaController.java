4dfde4c55e650866f0d8cc16b99a775bd770460b
Gabriel Fadul <gabrielgfadul022@gmail.com>
2025-12-17 17:57:01 -0300

    feat: Atualiza o controller com mock de dados

--- src/main/java/dev/java10x/cadastroDeNinjas/ninjas/controller/NinjaController.java
+++ src/main/java/dev/java10x/cadastroDeNinjas/ninjas/controller/NinjaController.java
@@ -1,8 +1,6 @@
 package dev.java10x.cadastroDeNinjas.ninjas.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping
@@ -13,4 +11,33 @@ public class NinjaController {
         return "Essa eh a minha primeira mensagem";
     }
 
+    // Adicionar Ninja (CREATE)
+    @PostMapping("/criar")
+    public String criarNinja(){
+        return "Ninja Criado";
+    }
+
+    // Mostrar todos os Ninjas (READ)
+    @GetMapping("/todos")
+    public String mostrarTodosOsNinjas(){
+        return "Ninja lido";
+    }
+
+
+    // Mostrar Ninja por ID (CREATE)
+    @GetMapping("/todosID")
+    public String mostrarTodosOsNinjasPorId(){
+        return "Ninja lido id";
+    }
+
+    // Alterar dados dos ninjas (UPDATE)
+    @PutMapping("/alterarId")
+    public String alterarNinjasPorId(){
+        return "Alterar Ninja ";
+    }
+    // Deletar Ninja (DELETE)
+    @DeleteMapping("/deletarID")
+    public String deletarNinjasPorId(){
+        return "Ninja deletado";
+    }
 }
